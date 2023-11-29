f31fd66d209043f5eeadbadbea4adbd1de4de2eb
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2023-11-29 18:04:47 +0300

    Added solution for Longest Common Prefix

---
+++ Python-Basics/longest-common-prefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        i = float("inf")
+        for j in strs:
+            i = min(i, len(j))
+        cout = ""
+        for k in range(i):
+            cur = strs[0][k]
+            flag = False
+            for l in strs:
+                if l[k] != cur:
+                    flag = True
+                    break
+            if flag:
+                break
+            cout += cur
+        return cout
