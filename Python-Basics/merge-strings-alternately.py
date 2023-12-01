926123cd3877a5671eafb8171c12bbec84130950
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2023-12-01 18:17:40 +0300

    Added solution for Merge Strings Alternately

---
+++ Python-Basics/merge-strings-alternately.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = 0 
+        cout = ""
+        while i < min(len(word1), len(word2)):
+            cout += word1[i]
+            cout += word2[i]
+            i += 1
+        cout += word1[i:]
+        cout += word2[i:]
+        return cout
