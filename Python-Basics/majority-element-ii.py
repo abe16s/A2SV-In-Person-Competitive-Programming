90bea81236174201f931dd88d5c1ec7fc1657a1e
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2023-12-06 17:21:02 +0300

    Added solution for Majority Element II

---
+++ Python-Basics/majority-element-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        atleast = len(nums)//3
+        output = set()
+        for num in nums:
+            if num not in output and nums.count(num) > atleast:
+                output.add(num)
+        return list(output)
