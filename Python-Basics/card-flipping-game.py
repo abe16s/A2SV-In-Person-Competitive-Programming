01a6245a52a242e73a782d1f8749110ec7b84c43
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2023-12-12 19:34:12 +0300

    Added solution for Card Flipping Game

---
+++ Python-Basics/card-flipping-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        onboth = set()
+        for i in range(len(fronts)):
+            if fronts[i] == backs[i]:
+                onboth.add(fronts[i])
+        mini = 2001
+        for i in range(len(fronts)):
+            if fronts[i] not in onboth:
+                mini = min(mini, fronts[i])
+            if backs[i] not in onboth:
+                mini = min(mini, backs[i]) 
+        return mini if mini < 2001 else 0
