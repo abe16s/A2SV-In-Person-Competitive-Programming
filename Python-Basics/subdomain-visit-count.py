f46ebb2861ed21ff81f909b000ecfd8daeec627d
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2023-12-14 21:14:02 +0300

    Added solution for Subdomain Visit Count

---
+++ Python-Basics/subdomain-visit-count.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        output = defaultdict(int)
+        for i in range(len(cpdomains)):
+            current = cpdomains[i].split(" ")
+            domains = current[1].split(".")
+            for j in range(len(domains)-1,-1,-1):
+                output[".".join(domains[j:])] += int(current[0])
+        return [str(output[key]) + " " + key for key in output]
