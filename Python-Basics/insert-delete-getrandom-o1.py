044e0fb38444caa8cdb6a4be64c4123c8a22eca8
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2023-12-18 20:51:07 +0300

    Added solution for Insert Delete GetRandom O(1)

---
+++ Python-Basics/insert-delete-getrandom-o1.py
@@ -0,0 +1,25 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.RandomizedSet = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.RandomizedSet:
+            return False
+        self.RandomizedSet.add(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.RandomizedSet:
+            return False
+        self.RandomizedSet.remove(val)
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.RandomizedSet))
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
