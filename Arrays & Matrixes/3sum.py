cee91c02f8786144004e89f15a59b587ceaf7228
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-01-04 18:28:20 +0300

    Added solution for 3Sum

---
+++ Arrays & Matrixes/3sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        cout = []
+        for i in range(len(nums)-2):
+            if i and nums[i] == nums[i-1]:
+                continue
+            target = 0 - nums[i]
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] == target:
+                    cout.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < len(nums) and nums[l] == nums[l-1]:
+                        l += 1
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
+        return cout
