bf4632184a36ac58e900823b1f4fd757e6f86cd0
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-03-01 14:20:21 +0300

    Add solution for Balance a Binary Search Tree

---
+++ Camp I/leetcode/balance-a-binary-search-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        nums = []
+        def inorder(cur):
+            if not cur:
+                return
+            inorder(cur.left)
+            nums.append(cur.val)
+            inorder(cur.right)
+        inorder(root)
+
+        def balance(start, end):
+            if start >= end:
+                return None
+            middle = start + (end-start) // 2
+            root = TreeNode(nums[middle])
+            root.left = balance(start, middle)
+            root.right = balance(middle+1, end)
+            return root
+        
+        return balance(0, len(nums))
+
+
+
