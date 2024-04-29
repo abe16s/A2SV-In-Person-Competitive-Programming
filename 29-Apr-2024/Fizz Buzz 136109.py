40f9648ca32b82ecd95442b4ecf2b1b4f6f923e4
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-04-29 22:05:04 +0300

    Created 'Fizz Buzz' by A2SV Hub

---
+++ 29-Apr-2024/Fizz Buzz 136109.py
@@ -0,0 +1,15 @@
+# Problem: Fizz Buzz - https://leetcode.com/problems/fizz-buzz/
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        out = []
+        for j in range(1,n+1):
+            if j%3 == 0 and j%5 == 0:
+                out.append("FizzBuzz")
+            elif j%3 == 0:
+                out.append("Fizz")
+            elif j%5 == 0:
+                out.append("Buzz")
+            else:
+                out.append(str(j))
+        return out
