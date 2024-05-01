34cc4e88b6e4761374e7662a53f8a7c9172deed2
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-01 21:24:56 +0300

    Created 'Permutations' by A2SV Hub

---
+++ 01-May-2024/Permutations 138661.py
@@ -0,0 +1,19 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutations = []
+        def per(cur):
+            if len(cur) == len(nums):
+                permutations.append(cur[:])
+
+            for i in range(len(nums)):
+                if nums[i] not in cur:
+                    cur.append(nums[i])
+                    per(cur)
+                    cur.pop()
+
+        # for i in range(len(nums)):
+        per([])
+        
+        return permutations
