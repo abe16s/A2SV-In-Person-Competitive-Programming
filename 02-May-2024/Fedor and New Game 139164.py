0a117275ae1e592a7587518f99541fcea6108f04
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-02 17:10:15 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 02-May-2024/Fedor and New Game 139164.py
@@ -0,0 +1,10 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = list(map(int, input().split()))
+players = [int(input()) for _ in range(m+1)]
+friends = 0
+for i in range(m):
+    diff = (players[-1] ^ players[i]).bit_count()
+    if diff <= k:
+        friends += 1
+print(friends)
