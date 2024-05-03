d69c431db55919f20d704b265ba2b37beccdc024
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-03 09:35:33 +0300

    Created 'Masha and Beautiful Tree' by A2SV Hub

---
+++ 03-May-2024/Masha and Beautiful Tree 139605.py
@@ -0,0 +1,23 @@
+# Problem: Masha and Beautiful Tree - https://codeforces.com/problemset/problem/1741/D
+
+def dc(nums):
+    global ctr
+    if len(nums) == 1:
+        return nums[0]
+    middle = len(nums)//2
+    l = dc(nums[:middle])
+    r = dc(nums[middle:])
+    if not l or not r or max(l,r) - min(l,r) > len(nums) - 1:
+        ctr = -1
+        return False
+    if l > r:
+        ctr += 1
+    return min(l,r)
+
+
+for t in range(int(input())):
+    ctr = 0
+    n = int(input())
+    nums = list(map(int, input().split()))
+    dc(nums)
+    print(ctr)
