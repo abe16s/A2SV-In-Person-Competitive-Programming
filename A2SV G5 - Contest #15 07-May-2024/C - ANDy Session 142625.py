06e47b102101a1b3cfdfac630762c23b299afe80
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-07 19:14:30 +0300

    Created 'C - ANDy Session' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/C - ANDy Session 142625.py
@@ -0,0 +1,15 @@
+# Problem: C - ANDy Session - https://codeforces.com/gym/522079/problem/C
+
+for t in range(int(input())):
+    n, k = map(int, input().split())
+    nums = list(map(int, input().split()))
+    ans = 0
+    for i in range(30, -1, -1):
+        unset = 0
+        for j in range(n):
+            if (nums[j] & (1<<i)) == 0:
+                unset += 1
+        if unset <= k:
+            k -= unset
+            ans |= (1<<i)
+    print(ans)
