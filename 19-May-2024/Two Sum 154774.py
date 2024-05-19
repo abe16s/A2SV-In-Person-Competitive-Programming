20f176580b0391b2500ec0977286ced1a2470639
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-19 16:52:40 +0300

    Created 'Two Sum' by A2SV Hub

---
+++ 19-May-2024/Two Sum 154774.py
@@ -0,0 +1,11 @@
+# Problem: Two Sum - https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        possible_values = {}
+        for i in range(len(nums)):
+            if nums[i] in possible_values:
+                return [i,possible_values[nums[i]]]
+            possible_values[target - nums[i]] = i
+        
+
