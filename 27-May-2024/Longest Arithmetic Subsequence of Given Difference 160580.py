df5ae2045f583719b0477a0f6b292cddfaa61664
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-27 19:27:13 +0300

    Created 'Longest Arithmetic Subsequence of Given Difference' by A2SV Hub

---
+++ 27-May-2024/Longest Arithmetic Subsequence of Given Difference 160580.py
@@ -0,0 +1,9 @@
+# Problem: Longest Arithmetic Subsequence of Given Difference - https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/description/
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        dp = defaultdict(int)
+        for i in range(len(arr)-1, -1,-1):
+            dp[arr[i]] = max(dp[arr[i]+difference] + 1, dp[arr[i]])
+
+        return max(dp.values())
