7e746623a54cceef73f235ca8fe0cabcf55e00df
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-05-29 18:55:32 +0300

    Created 'Max Consecutive Ones' by A2SV Hub

---
+++ 29-May-2024/Max Consecutive Ones 161739.py
@@ -0,0 +1,13 @@
+# Problem: Max Consecutive Ones - https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        maxi = 0
+        cur = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                cur += 1
+                maxi = max(maxi, cur)
+            else:
+                cur = 0
+        return maxi
