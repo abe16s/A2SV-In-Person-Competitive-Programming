abab0c532b53154dd13d2a8d088ae4c6df963281
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-06-18 20:14:22 +0300

    Created 'Keyboard Row' by A2SV Hub

---
+++ 18-Jun-2024/Keyboard Row 169613.py
@@ -0,0 +1,20 @@
+# Problem: Keyboard Row - https://leetcode.com/problems/keyboard-row/description/
+
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+        cout = []
+        for w in words:
+            flag = True
+            temp = w
+            w = w.lower()
+            for i in range(3):
+                if w[0] in rows[i]:
+                    break
+            for j in range(1,len(w)):
+                if w[j] not in rows[i]:
+                    flag = False
+                    break
+            if flag:
+                cout.append(temp)
+        return cout            
