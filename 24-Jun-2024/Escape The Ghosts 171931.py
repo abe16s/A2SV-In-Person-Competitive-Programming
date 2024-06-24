bfe5bbf24f135d94dcaa7e37a35e3624e6993a0b
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-06-24 08:05:02 +0300

    Created 'Escape The Ghosts' by A2SV Hub

---
+++ 24-Jun-2024/Escape The Ghosts 171931.py
@@ -0,0 +1,12 @@
+# Problem: Escape The Ghosts - https://leetcode.com/problems/escape-the-ghosts/
+
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        target_dist = abs(target[0]) + abs(target[1])
+        
+        for r, c in ghosts:
+            ghost_target = abs(target[0] - r) + abs(target[1] - c)
+            if ghost_target <= target_dist:
+                return False
+            
+        return True
