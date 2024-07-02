9ec0c4d39f9290edce1d90d9a826693040b237d9
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-07-02 18:21:47 +0300

    Created 'Intersection of Two Arrays II' by A2SV Hub

---
+++ 02-Jul-2024/Intersection of Two Arrays II 175586.py
@@ -0,0 +1,22 @@
+# Problem: Intersection of Two Arrays II - https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i = j =  0
+        output = []
+        while i < len(nums1):
+            a = 1
+            cur = nums1[i]
+            while i+1 < len(nums1) and nums1[i+1] == nums1[i]:
+                a += 1
+                i += 1
+            b = 0
+            while j < len(nums2) and nums2[j] <= cur:
+                if nums2[j] == cur:
+                    b += 1
+                j += 1
+            output.extend([cur]*min(a,b))
+            i += 1
+        return output
