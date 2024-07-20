acaa337a502029d9004e58d03815dbd20294d862
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-07-20 06:42:39 +0300

    Created 'Corporate Flight Bookings' by A2SV Hub

---
+++ 20-Jul-2024/Corporate Flight Bookings 182074.py
@@ -0,0 +1,13 @@
+# Problem: Corporate Flight Bookings - https://leetcode.com/problems/corporate-flight-bookings/
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        ps = [0] * (n+1)
+        for f, l, s in bookings:
+            ps[f-1] += s
+            ps[l] -= s
+
+        for i in range(1, n+1):
+            ps[i] += ps[i-1]
+
+        return ps[:n] 
