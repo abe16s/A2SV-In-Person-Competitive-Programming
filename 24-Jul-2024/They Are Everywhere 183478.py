576050dc56fc63d8a767b84a96116c2820acb1dc
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-07-24 07:28:43 +0300

    Created 'They Are Everywhere' by A2SV Hub

---
+++ 24-Jul-2024/They Are Everywhere 183478.py
@@ -0,0 +1,19 @@
+# Problem: They Are Everywhere - https://codeforces.com/problemset/problem/701/C
+
+from collections import defaultdict
+n = int(input())
+s = input()
+flats = float("inf")
+pokemons = set(s)
+cur = defaultdict(int)
+l = 0
+for r in range(n):
+    cur[s[r]] += 1
+    while len(cur) == len(pokemons):
+        flats = min(flats, r-l+1)     
+        if cur[s[l]] > 1:
+            cur[s[l]] -= 1
+            l += 1
+        else:
+            break
+print(flats)
