2e8b35b7fc3bed2af05cf58aa5f64e94c1b11ce3
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-07-31 07:33:38 +0300

    Created 'fruit-into-baskets' by A2SV Hub

---
+++ 31-Jul-2024/fruit-into-baskets 186178.py
@@ -0,0 +1,27 @@
+# Problem: fruit-into-baskets - https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        if len(fruits)<=2:
+            return len(fruits)
+        count = defaultdict(int)
+        start = 0
+        end = 0
+        maxi = 0
+        while end < len(fruits):
+            if len(count) == 2 and fruits[end] not in count:
+                maxi = max(maxi, end-start)
+                while 0 not in count.values():
+                    count[fruits[start]] -= 1
+                    start += 1
+                for k in count:
+                    if count[k] == 0:
+                        count.pop(k)
+                        break
+
+            count[fruits[end]] += 1
+            end += 1
+        
+        maxi = max(maxi, end-start)
+
+        return maxi
