35a543d30e6975c639eb21b686ee14d32de4c9b9
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-08-03 10:39:46 +0300

    Created 'C - Casino' by A2SV Hub

---
+++ A2SV G5 - Contest #10 03-Aug-2024/C - Casino 187025.py
@@ -0,0 +1,16 @@
+# Problem: C - Casino - https://codeforces.com/gym/514644/problem/C
+
+n = int(input())
+nums = list(map(int, input().split()))
+lcm = nums[0]
+for i in range(n):
+    while nums[i] % 2 == 0:
+        nums[i] //= 2
+
+    while nums[i] % 3 == 0:
+        nums[i] //= 3
+    
+if nums.count(nums[0]) != n:
+    print("No")
+else:
+    print("Yes")
