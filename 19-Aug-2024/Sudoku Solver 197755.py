fd396acba0786311692f7d4ee1e3124f3ae616c0
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-08-19 07:01:54 +0300

    Created 'Sudoku Solver' by A2SV Hub

---
+++ 19-Aug-2024/Sudoku Solver 197755.py
@@ -0,0 +1,37 @@
+# Problem: Sudoku Solver - https://leetcode.com/problems/sudoku-solver/description/
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        cells = defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    cells[(r//3, c//3)].add(board[r][c])
+
+        def search():
+            flag = True
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == ".":
+                        flag = False
+                        break
+                if not flag:
+                    break
+
+            if flag:
+                return True
+            
+            for i in range(1,10):
+                i = str(i)
+                if i not in board[r] and i not in cells[(r//3,c//3)] and i not in [board[_][c] for _ in range(9)]:
+                    board[r][c] = i
+                    cells[(r//3,c//3)].add(i)
+                    temp = search()
+                    if temp:
+                        return True
+                    board[r][c] = "."
+                    cells[(r//3,c//3)].remove(i)
+        search()
