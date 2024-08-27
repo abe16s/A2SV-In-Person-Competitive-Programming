4730b4596281a730803b24e8e9728ba281c3201a
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-08-27 08:00:30 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 27-Aug-2024/New Year Transportation 208233.py
@@ -0,0 +1,13 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+cur = 0
+
+while cur <= t-1:
+    cur += a[cur]
+    if cur == t-1:
+        print("YES")
+        break
+else:
+    print("NO")
