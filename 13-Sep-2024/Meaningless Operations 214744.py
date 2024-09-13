801b38a711ef495767d6802a7784accf1004ac58
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-09-13 07:49:15 +0300

    Created 'Meaningless Operations' by A2SV Hub

---
+++ 13-Sep-2024/Meaningless Operations 214744.py
@@ -0,0 +1,28 @@
+# Problem: Meaningless Operations - https://codeforces.com/problemset/problem/1110/C
+
+import math
+
+def invertBits(n, length):     
+    m = 1 << length
+    m = m | m - 1
+    n = n ^ m
+    return n
+    
+for q in range(int(input())):
+    a = int(input())
+    max_gcd = -1
+    x = int(math.log(a, 2))
+    if invertBits(a, x) != 0:
+        m = 1 << x
+        m = m | m - 1
+        print(m)
+    else:
+        d = 2
+        while d*d <= a:
+            if a % d == 0:
+                break
+            d += 1
+        if a % d == 0:
+            print(max(d, a//d))
+        else:
+            print(1)
