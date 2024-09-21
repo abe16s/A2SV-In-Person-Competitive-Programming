d134ec74501bc7c16bbc0517ade04f037b7dfcb0
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-09-21 07:51:01 +0300

    Created 'Merge Interval' by A2SV Hub

---
+++ 21-Sep-2024/Merge Interval 216162.py
@@ -0,0 +1,17 @@
+# Problem: Merge Interval - https://leetcode.com/problems/merge-intervals/
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        cout = []
+        i = 0
+        while i < len(intervals):
+            curr = [intervals[i][0]]
+            maxi = intervals[i][-1] 
+            i += 1
+            while i < len(intervals) and maxi >= intervals[i][0]:
+                maxi = max(maxi, intervals[i][-1])
+                i += 1 
+            curr.append(maxi)
+            cout.append(curr)
+        return cout
