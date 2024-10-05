357bec993a1450cd61f226116a5913f26ed34cc9
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-10-05 06:24:08 +0300

    Created 'Permutation in String' by A2SV Hub

---
+++ 05-Oct-2024/Permutation in String 218164.py
@@ -0,0 +1,19 @@
+# Problem: Permutation in String - https://leetcode.com/problems/permutation-in-string/
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        c = collections.Counter(s1)
+        k = len(s1)
+        cur = collections.Counter(s2[:k])
+        if cur == c:
+            return True
+        i = k
+        while i < len(s2):
+            cur[s2[i-k]] -= 1
+            if cur[s2[i-k]] <= 0:
+                cur.pop(s2[i-k])
+            cur[s2[i]] += 1
+            if cur == c:
+                return True
+            i += 1
+        return False
