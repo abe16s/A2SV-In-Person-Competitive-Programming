d821e56b5c81ec93147fd4d60fbf46b9b49dae31
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-10-07 09:09:24 +0300

    Created 'My Calendar II' by A2SV Hub

---
+++ 07-Oct-2024/My Calendar II 218525.py
@@ -0,0 +1,16 @@
+# Problem: My Calendar II - https://leetcode.com/problems/my-calendar-ii/
+
+class MyCalendarTwo:
+    def __init__(self):
+        self.overlaps = []
+        self.calendar = []
+
+    def book(self, start, end):
+        for i, j in self.overlaps:
+            if start < j and end > i:
+                return False
+        for i, j in self.calendar:
+            if start < j and end > i:
+                self.overlaps.append((max(start, i), min(end, j)))
+        self.calendar.append((start, end))
+        return True
