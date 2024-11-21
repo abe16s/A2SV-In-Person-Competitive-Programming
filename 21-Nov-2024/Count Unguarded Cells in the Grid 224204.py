e06f566b314aa98745ff8c8b242c977bb7d971fb
Abenezer Seifu Dula <119857906+abe16s@users.noreply.github.com>
2024-11-21 08:35:05 +0300

    Created 'Count Unguarded Cells in the Grid' by A2SV Hub

---
+++ 21-Nov-2024/Count Unguarded Cells in the Grid 224204.py
@@ -0,0 +1,34 @@
+# Problem: Count Unguarded Cells in the Grid - https://leetcode.com/problems/count-unguarded-cells-in-the-grid/
+
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        cells = [["." for _ in range(n)] for _ in range(m)]
+        for wx, wy in walls:
+            cells[wx][wy] = "W"
+        #horizontal
+        for g in guards:
+            gy, gx = g[0], g[1]+1
+            while gx < n and cells[gy][gx] == ".":
+                cells[gy][gx] = "H"
+                gx += 1
+            gx = g[1]
+            while gx >= 0 and cells[gy][gx] == ".":
+                cells[gy][gx] = "H"
+                gx -= 1
+        #vertical 
+        for g in guards:
+            gy, gx = g[0]+1, g[1]
+            while gy < m and cells[gy][gx] in ".H":
+                cells[gy][gx] = "V"
+                gy += 1
+            gy = g[0]
+            while gy >= 0 and cells[gy][gx] in ".H":
+                cells[gy][gx] = "V"
+                gy -= 1
+        
+        output = 0
+        for r in range(m):
+            for c in range(n):
+                if cells[r][c] ==".":
+                    output += 1
+        return output
